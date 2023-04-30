8c604b6a35d3634f9e36fe01d43ae204e32e4d9d
SammyLeths <s.o.eyiowuawi@gmail.com>
2023-04-30 18:20:53 +0100

    added events filters

--- src/components/layout/main-header.js
+++ src/components/layout/main-header.js
@@ -6,7 +6,7 @@ const MainHeader = () => {
   return (
     <header className={classes.header}>
       <div className={classes.logo}>
-        <Link href='/'>Next Events</Link>
+        <Link href='/'>NextEvents</Link>
       </div>
       <nav className={classes.navigation}>
         <ul>
--- src/pages/events/[...slug].js
+++ src/pages/events/[...slug].js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useRouter } from 'next/router';
 import { getFilteredEvents } from '../../../dummy-data';
 import EventList from '@/components/events/event-list';
+import ResultsTitle from '@/components/events/results-title';
+import Button from '@/components/ui/button';
+import ErrorAlert from '@/components/ui/error-alert';
 
 const FilteredEventsPage = () => {
   const router = useRouter();
@@ -23,7 +26,17 @@ const FilteredEventsPage = () => {
     filteredMonth < 1 ||
     filteredMonth > 12
   ) {
-    return <p>Invalid filter. Please adjust your values!</p>;
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
+
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
   const filteredEvents = getFilteredEvents({
@@ -32,13 +45,26 @@ const FilteredEventsPage = () => {
   });
 
   if (!filteredEvents || filteredEvents.length === 0) {
-    return <p>No events found for the chosen filter!</p>;
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
+  const date = new Date(filteredYear, filteredMonth - 1);
+
   return (
-    <div>
+    <Fragment>
+      <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
-    </div>
+    </Fragment>
   );
 };
 
