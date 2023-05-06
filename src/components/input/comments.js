b7df94e6c2e8db7013d2b0223e00cdbfac0e91a6
SammyLeths <s.o.eyiowuawi@gmail.com>
2023-05-06 13:10:37 +0100

    added feedback notifications via context api

--- src/components/input/comments.js
+++ src/components/input/comments.js
@@ -1,21 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
+import NotificationContext from '@/store/notification-context';
 
 function Comments(props) {
   const { eventId } = props;
 
+  const notificationCtx = useContext(NotificationContext);
+
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const [isFetchingComments, setIsFetchingComment] = useState(false);
 
   useEffect(() => {
     if (showComments) {
+      setIsFetchingComment(true);
       fetch('/api/comments/' + eventId)
         .then((response) => response.json())
         .then((data) => {
           setComments(data.comments);
+          setIsFetchingComment(false);
         });
     }
   }, [showComments, eventId]);
@@ -25,6 +31,12 @@ function Comments(props) {
   }
 
   function addCommentHandler(commentData) {
+    notificationCtx.showNotification({
+      title: 'Sending comment...',
+      message: 'Your comment is currently being stored into a dtatbase',
+      status: 'pending',
+    });
+
     // send data to API
     fetch('/api/comments/' + eventId, {
       method: 'POST',
@@ -33,8 +45,29 @@ function Comments(props) {
         'Content-Type': 'application/json',
       },
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        return response.json().then((data) => {
+          throw new Error(data.message || 'Something went wrong!');
+        });
+      })
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: 'Success!',
+          message: 'Your comment was saved!',
+          status: 'success',
+        });
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: 'Error!',
+          message: error.message || 'Your comment was saved!',
+          status: 'error',
+        });
+      });
   }
 
   return (
@@ -43,7 +76,8 @@ function Comments(props) {
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments} />}
+      {showComments && !isFetchingComments && <CommentList items={comments} />}
+      {showComments && isFetchingComments && <p>Loading...</p>}
     </section>
   );
 }
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import MainHeader from './main-header';
+import Notification from '../ui/notification';
+import NotificationContext from '@/store/notification-context';
 
 const Layout = (props) => {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </Fragment>
   );
 };
